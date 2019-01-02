b0f336112120602c0a23fd428231903f67ee378c
Edward Barton <edward.barton@icloud.com>
2019-01-02 18:27:49 -0500

    Register view with vuetify styling

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <h1>REGISTER VIEW</h1>
-    <input v-model="email" type="email" name="email" id="email" placeholder="Email">
-    <br>
-    <input v-model="password" type="password" name="password" id="password" placeholder="Password">
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense dark class="cyan">
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <input v-model="email" type="email" name="email" id="email" placeholder="Email">
+            <br>
+            <input
+              v-model="password"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            >
+            <br>
+            <div class="error" v-html="error"></div>
+            <br>
+            <v-btn class="cyan" @click="register">Register</v-btn>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -17,16 +36,27 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      error: null
     };
   },
   methods: {
     async register() {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      });
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (err) {
+        this.error = err.response.data.error;
+      }
     }
   }
 };
 </script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
